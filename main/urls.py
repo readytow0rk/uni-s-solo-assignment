8e9767fa81b7e8c88750c756bc4fdf7e633f443a
Nikita <119955335+readytow0rk@users.noreply.github.com>
2025-04-15 13:08:39 +0100

    add appointment's manage links

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('book/', views.book, name='book'),
+    path('manage/', views.manage, name='manage'),
+    path('register/', views.register, name='register'),
 ]
+
--- main/views.py
+++ main/views.py
@@ -2,3 +2,14 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def book(request):
+    return render(request, 'book.html')
+
+def manage(request):
+    return render(request, 'manage.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+    
